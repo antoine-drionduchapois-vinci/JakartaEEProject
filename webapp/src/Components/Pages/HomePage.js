c05803776ede3748313b048905aa815d64c112ea
Antoine Drion <antoine.drionduchapoi@student.vinci.be>
2024-02-16 17:54:12 +0100

    feat:add homepage frontend

--- webapp/src/Components/Pages/HomePage.js
+++ webapp/src/Components/Pages/HomePage.js
@@ -1,6 +1,33 @@
+import Navigate from '../Router/Navigate';
+
 const HomePage = () => {
   const main = document.querySelector('main');
-  main.innerHTML = `<h3>Welcome to your home page!</h3>`;
+
+  function register() {
+    console.log("Register Page");
+    Navigate('/register');
+  }
+
+  function connect() {
+    console.log("Connection Page");
+    Navigate('/login');
+  }
+  
+
+  const bloc1 = `
+    <h1>StageLink IPL</h1>
+    <h2>Bienvenue sur StageLink IPL</h2>
+    <p>L’application qui vous permet de gérer vos recherches de stage</p>
+
+    <button id="registerButton">S'inscrire</button>
+    <button id="connectButton">Se connecter</button>
+  `;
+
+  main.innerHTML = bloc1;
+
+
+  document.getElementById('registerButton').addEventListener('click', register);
+  document.getElementById('connectButton').addEventListener('click', connect);
 };
 
 export default HomePage;
