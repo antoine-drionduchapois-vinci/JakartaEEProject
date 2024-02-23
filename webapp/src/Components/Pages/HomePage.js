23b23cfbfa5820551a412cc2bfb3eba596143055
Drion Antoine <antoine.drionduchapoi@student.vinci.be>
2024-02-23 15:48:27 +0100

    feat:adding tailwind css

--- webapp/src/Components/Pages/HomePage.js
+++ webapp/src/Components/Pages/HomePage.js
@@ -16,12 +16,14 @@ const HomePage = () => {
   
 
   const bloc1 = `
-    <h1>StageLink IPL</h1>
-    <h2>Bienvenue sur StageLink IPL</h2>
-    <p>L’application qui vous permet de gérer vos recherches de stage</p>
+  <h1 class="text-3xl font-bold underline">StageLink IPL</h1>
+<h2 class="text-xl font-semibold mt-4">Bienvenue sur StageLink IPL</h2>
+<p class="mt-2">L’application qui vous permet de gérer vos recherches de stage</p>
 
-    <button id="registerButton">S'inscrire</button>
-    <button id="connectButton">Se connecter</button>
+<button id="registerButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">S'inscrire</button>
+<button id="connectButton" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-2">Se connecter</button>
+
+
   `;
 
   main.innerHTML = bloc1;
