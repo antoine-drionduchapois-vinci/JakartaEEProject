5d759dfb30af41372e7c6291ad907eceeed9f316
antoine <antoine.drionduchapoi@student.vinci.be>
2024-02-27 23:08:47 +0100

    feat:adding javadoc

--- src/main/java/be/vinci/pae/api/filters/CorsFilter.java
+++ src/main/java/be/vinci/pae/api/filters/CorsFilter.java
@@ -5,18 +5,22 @@ import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 
+/**
+ * The CorsFilter class is a response filter that adds the necessary headers to responses to handle
+ * CORS (Cross-Origin Resource Sharing) requests.
+ */
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
-  @Override
-  public void filter(ContainerRequestContext requestContext,
-      ContainerResponseContext responseContext) {
-    responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-    responseContext.getHeaders().add("Access-Control-Allow-Headers",
-        "origin, content-type, accept, authorization");
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+        ContainerResponseContext responseContext) {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers",
+            "origin, content-type, accept, authorization");
 
-    responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-    responseContext.getHeaders().add("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-  }
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods",
+            "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
 }
