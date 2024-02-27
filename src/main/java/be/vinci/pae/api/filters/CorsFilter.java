afc66c95fbb45a81dae52f957c9f2fa47ce32932
antoine <antoine.drionduchapoi@student.vinci.be>
2024-02-27 23:13:55 +0100

    fix:indetation corsfilter

--- src/main/java/be/vinci/pae/api/filters/CorsFilter.java
+++ src/main/java/be/vinci/pae/api/filters/CorsFilter.java
@@ -12,15 +12,15 @@ import jakarta.ws.rs.ext.Provider;
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
-    @Override
-    public void filter(ContainerRequestContext requestContext,
-        ContainerResponseContext responseContext) {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers",
-            "origin, content-type, accept, authorization");
+  @Override
+  public void filter(ContainerRequestContext requestContext,
+      ContainerResponseContext responseContext) {
+    responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+    responseContext.getHeaders().add("Access-Control-Allow-Headers",
+        "origin, content-type, accept, authorization");
 
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods",
-            "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-    }
+    responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+    responseContext.getHeaders().add("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+  }
 }
