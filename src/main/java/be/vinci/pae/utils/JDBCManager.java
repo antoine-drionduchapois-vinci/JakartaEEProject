fa382e647e1441573dad2b4b0de542690ec6d568
diego rousseaux <diego.rousseaux@student.vinci.be>
2024-02-28 14:30:21 +0100

    fix: missing javadoc

--- src/main/java/be/vinci/pae/utils/JDBCManager.java
+++ src/main/java/be/vinci/pae/utils/JDBCManager.java
@@ -3,9 +3,23 @@ package be.vinci.pae.utils;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Interface for managing database connections.
+ */
 public interface JDBCManager {
 
+  /**
+   * Retrieves a connection to the database.
+   *
+   * @return The database connection.
+   * @throws SQLException if a database access error occurs
+   */
   Connection getConnection() throws SQLException;
 
+  /**
+   * Closes the database connection.
+   *
+   * @throws SQLException if a database access error occurs
+   */
   void close() throws SQLException;
 }
