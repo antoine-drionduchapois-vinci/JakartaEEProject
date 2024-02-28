afc7dddec3f9afd21543166142901716a833445a
Alexis Cattaruzza <alexis.cattaruzza@student.vinci.be>
2024-02-28 12:37:05 +0100

    refactor: add javadoc and indentation

--- src/main/java/be/vinci/pae/utils/JDBCManager.java
+++ src/main/java/be/vinci/pae/utils/JDBCManager.java
@@ -3,6 +3,9 @@ package be.vinci.pae.utils;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * The JDBCManager interface defines methods for managing JDBC connections to a database.
+ */
 public interface JDBCManager {
 
   Connection getConnection() throws SQLException;
