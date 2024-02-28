cb1f3877de3301dce6864535d66951f8bab70fd7
diego rousseaux <diego.rousseaux@student.vinci.be>
2024-02-28 14:27:18 +0100

    fix: missing javadoc

--- src/main/java/be/vinci/pae/utils/JDBCManagerImpl.java
+++ src/main/java/be/vinci/pae/utils/JDBCManagerImpl.java
@@ -4,13 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Implementation of JDBCManager interface for managing database connections.
+ */
 public class JDBCManagerImpl implements JDBCManager {
 
   private Connection connection;
-  private final String URL = Config.getProperty("db.url");
-  private final String USERNAME = Config.getProperty("db.username");
-  private final String PASSWORD = Config.getProperty("db.password");
+  private final String URL = Config.getProperty("db.url"); // URL for the database
+  private final String USERNAME = Config.getProperty("db.username"); // Username for database access
+  private final String PASSWORD = Config.getProperty("db.password"); // Password for database access
 
+  /**
+   * Retrieves a connection to the database.
+   *
+   * @return The database connection.
+   * @throws SQLException if a database access error occurs or the URL is null
+   */
   @Override
   public Connection getConnection() throws SQLException {
     if (connection == null || connection.isClosed()) {
@@ -20,6 +29,11 @@ public class JDBCManagerImpl implements JDBCManager {
     return connection;
   }
 
+  /**
+   * Closes the database connection.
+   *
+   * @throws SQLException if a database access error occurs
+   */
   @Override
   public void close() throws SQLException {
     if (connection != null && !connection.isClosed()) {
