0b374d349f31ba8e2db28dd91dbb7d31d4aba051
Antoine Drion <antoine.drionduchapoi@student.vinci.be>
2024-03-13 10:01:51 +0100

    feat: getEntrpriseByUserId

--- src/main/java/be/vinci/pae/api/EnterpriseDAOImpl.java
+++ src/main/java/be/vinci/pae/api/EnterpriseDAOImpl.java
@@ -40,7 +40,8 @@ public class EnterpriseDAOImpl implements EnterpriseDAO {
         String avisProfesseur = rs.getString("avis_professeur");
 
         // Create a new EnterpriseImpl object and add it to the list
-        Enterprise enterprise = new EnterpriseImpl(entrepriseId, nom, appellation, adresse, telephone, isBlacklist, avisProfesseur);
+        Enterprise enterprise = new EnterpriseImpl(entrepriseId, nom, appellation, adresse,
+            telephone, isBlacklist, avisProfesseur);
         enterprises.add(enterprise);
       }
     } catch (SQLException e) {
@@ -50,4 +51,54 @@ public class EnterpriseDAOImpl implements EnterpriseDAO {
 
     return enterprises;
   }
+
+
+  /**
+   * Retrieves the entrprise that corresponds to the users internship.
+   *
+   * @return enterprises.
+   */
+  @Override
+  public Enterprise getEnterpriseById(int id) {
+    PreparedStatement ps = myDalService
+        .getPS(
+            "SELECT * FROM projetae.entreprises e, projetae.stages s WHERE s.entreprise = e.entreprise_id AND s.utilisateur = ?");
+    try {
+      ps.setInt(1, id);
+      ps.execute();
+
+      try (ResultSet rs = ps.getResultSet()) {
+        // Check if ResultSet has data
+        if (rs.next()) {
+          int entrepriseId = rs.getInt("entreprise_id");
+          String nom = rs.getString("nom");
+          String appellation = rs.getString("appellation");
+          String adresse = rs.getString("adresse");
+          String telephone = rs.getString("telephone");
+          boolean isBlacklist = rs.getBoolean("is_blacklist");
+          String avisProfesseur = rs.getString("avis_professeur");
+
+          Enterprise enterprise = new EnterpriseImpl(entrepriseId, nom, appellation, adresse,
+              telephone, isBlacklist, avisProfesseur);
+          if (enterprise == null) {
+            System.out.println("Entreprise is NULL");
+          }
+          System.out.println("entreprise : ");
+          System.out.println(enterprise);
+          return enterprise;
+        } else {
+          // Handle the case where no enterprise is found for the user
+          System.out.println("RS.next == NULL");
+          return null;
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    // If there's an exception or no data, return null
+    return null;
+  }
+
 }
