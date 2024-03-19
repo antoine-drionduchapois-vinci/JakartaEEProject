185f5cdd30d3b74728881e4623ef7fe2f6c4baf2
Alexis Cattaruzza <alexis.cattaruzza@student.vinci.be>
2024-03-19 12:20:56 +0100

    test: userUCC all methods

--- src/test/java/be/vinci/pae/ucc/UserUCCImplTest.java
+++ src/test/java/be/vinci/pae/ucc/UserUCCImplTest.java
@@ -1,13 +1,17 @@
 package be.vinci.pae.ucc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import be.vinci.pae.TestBinder;
 import be.vinci.pae.dao.UserDAO;
+import be.vinci.pae.domain.UserDTO;
 import be.vinci.pae.utils.Config;
+import java.util.ArrayList;
+import java.util.List;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,5 +41,57 @@ class UserUCCImplTest {
 
     assertEquals(5, result);
     verify(userDAO, times(1)).getStudentsWithoutStage();
+  }
+
+  @Test
+  void countStudents() {
+    // Arrange
+    when(userDAO.getTotalStudents()).thenReturn(10);
+
+    // Act
+    int result = userUCC.countStudents();
+
+    // Assert
+    assertEquals(10, result);
+    verify(userDAO, times(1)).getTotalStudents();
+  }
+
+  @Test
+  void getUsersAsJson() {
+    // Arrange
+    List<UserDTO> userList = new ArrayList<>();
+    UserDTO user1 = mock(UserDTO.class);
+    when(user1.getSurname()).thenReturn("John");
+    when(user1.getName()).thenReturn("Doe");
+    UserDTO user2 = mock(UserDTO.class);
+    when(user2.getSurname()).thenReturn("Jane");
+    when(user2.getName()).thenReturn("Smith");
+    userList.add(user1);
+    userList.add(user2);
+    when(userDAO.getAllStudents()).thenReturn(userList);
+
+    // Act
+    List<UserDTO> result = userUCC.getUsersAsJson();
+
+    // Assert
+    assertEquals(userList, result);
+    verify(userDAO, times(1)).getAllStudents();
+  }
+
+  @Test
+  void getUsersByIdAsJson() {
+    // Arrange
+    int userId = 123;
+    UserDTO user = mock(UserDTO.class);
+    when(user.getSurname()).thenReturn("John");
+    when(user.getName()).thenReturn("Doe");
+    when(userDAO.getOneByID(userId)).thenReturn(user);
+
+    // Act
+    UserDTO result = userUCC.getUsersByIdAsJson(userId);
+
+    // Assert
+    assertEquals(user, result);
+    verify(userDAO, times(1)).getOneByID(userId);
   }
 }
