9bedf692d46097f1a0da761d5f6f2c52927e9334
Antoine Drion <antoine.drionduchapoi@student.vinci.be>
2024-03-19 10:16:29 +0100

    feat: adding contactUCC

---
+++ src/main/java/be/vinci/pae/ucc/ContactUCC.java
@@ -0,0 +1,9 @@
+package be.vinci.pae.ucc;
+
+import be.vinci.pae.domain.ContactDTO;
+import java.util.List;
+
+public interface ContactUCC {
+
+  List<ContactDTO> getAllUsersContact(int id);
+}
---
+++ src/main/java/be/vinci/pae/ucc/ContactUCCImpl.java
@@ -0,0 +1,25 @@
+package be.vinci.pae.ucc;
+
+import be.vinci.pae.dao.ContactDAO;
+import be.vinci.pae.domain.ContactDTO;
+import jakarta.inject.Inject;
+import java.util.List;
+
+/**
+ * Implementation of the Contact UCC interface.
+ */
+public class ContactUCCImpl implements ContactUCC {
+
+  @Inject
+  private ContactDAO myContactDAO;
+
+  @Override
+  public List<ContactDTO> getAllUsersContact(int id) {
+
+    // Récupérer la liste complète des contacts de user depuis votre DAO
+    List<ContactDTO> getAllUsersContact = myContactDAO.getAllUsersContact(id);
+
+    return getAllUsersContact;
+  }
+
+}
