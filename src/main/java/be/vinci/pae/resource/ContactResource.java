4c23e2f6a1dd6dce2b438eb58afe89c2a576b7cc
Josué <josue.dubois@student.vinci.be>
2024-03-20 09:31:57 +0100

    feat: add meet enterprise route and logic

--- src/main/java/be/vinci/pae/resource/ContactResource.java
+++ src/main/java/be/vinci/pae/resource/ContactResource.java
@@ -201,4 +201,23 @@ public class ContactResource {
 
     return contact;
   }
+
+  @POST
+  @Path("/meet")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public ObjectNode meet(JsonNode json) {
+    if (!json.hasNonNull("contactId") || !json.hasNonNull("meetingPoint")) {
+      throw new WebApplicationException("contactId, meetingPoint required", Status.BAD_REQUEST);
+    }
+
+    ObjectNode contact = myContactUCC.meetEnterprise(json.get("contactId").asInt(),
+        json.get("meetingPoint").asText());
+
+    if (contact == null) {
+      throw new WebApplicationException("not found", Status.NOT_FOUND);
+    }
+
+    return contact;
+  }
 }
