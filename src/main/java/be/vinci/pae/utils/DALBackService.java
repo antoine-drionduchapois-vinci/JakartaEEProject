4262af5609d2af81d063cce260b8797e315a114a
Mwana-Nteba Yohani <yohani.mwananteba@student.vinci.be>
2024-03-22 10:23:24 +0100

    refactor: adding javadoc for DALBackService

--- src/main/java/be/vinci/pae/utils/DALBackService.java
+++ src/main/java/be/vinci/pae/utils/DALBackService.java
@@ -2,6 +2,10 @@ package be.vinci.pae.utils;
 
 import java.sql.PreparedStatement;
 
+/**
+ * DALBackService is an interface providing a method for obtaining a prepared statement for the
+ * provided SQL query.
+ */
 public interface DALBackService {
 
   /**
@@ -9,7 +13,7 @@ public interface DALBackService {
    *
    * @param sql the SQL query
    * @return a prepared statement for the SQL query
+   * @throws RuntimeException if a database access error occurs
    */
   PreparedStatement getPS(String sql);
-
 }
