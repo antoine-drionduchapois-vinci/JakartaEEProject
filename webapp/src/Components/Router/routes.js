27bf7da4aee318df26dce5fc7b69c9a1297dd996
Antoine Drion <antoine.drionduchapoi@student.vinci.be>
2024-03-25 11:10:46 +0100

    fix: removing unused dashboard

--- webapp/src/Components/Router/routes.js
+++ webapp/src/Components/Router/routes.js
@@ -2,7 +2,6 @@ import HomePage from '../Pages/HomePage';
 import Logout from '../Logout/Logout';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
-import Dashboard from '../Pages/Dashboard';
 import DashboardTeacher from '../Pages/DashboardTeacher';
 import Contact from '../Pages/Contact';
 import DashboardStudent from '../Pages/DashboardStudent';
@@ -12,7 +11,6 @@ const routes = {
   '/login': LoginPage,
   '/register': RegisterPage,
   '/logout': Logout,
-  '/dashboard': Dashboard,
   '/dashboardT': DashboardTeacher,
   '/contact': Contact,
   '/dashboardS': DashboardStudent
