a938a7310264d3cf759ee19bd9f752bbfe3bdc6f
RousseauxDiego <diegorousseaux54@gmail.com>
2024-03-25 13:38:57 +0100

    fix:jenkins

--- src/main/java/be/vinci/pae/resources/JWT.java
+++ src/main/java/be/vinci/pae/resources/JWT.java
@@ -3,7 +3,16 @@ package be.vinci.pae.resources;
 import be.vinci.pae.domain.UserDTO;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+/**
+ * Interface for JWT (JSON Web Token) operations.
+ */
 public interface JWT {
 
+  /**
+   * Creates a JWT token for the given user DTO.
+   *
+   * @param userDTO the user DTO for which the token is created
+   * @return an ObjectNode containing the JWT token and user information
+   */
   ObjectNode createToken(UserDTO userDTO);
 }
--- src/main/java/be/vinci/pae/resources/JWTImpl.java
+++ src/main/java/be/vinci/pae/resources/JWTImpl.java
@@ -6,15 +6,31 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+/**
+ * Implementation of JWT (JSON Web Token) interface.
+ */
 public class JWTImpl implements JWT {
+
+  // Algorithm used for JWT token generation
   private final Algorithm jwtAlgorithm = Algorithm.HMAC256(Config.getProperty("JWTSecret"));
+  // ObjectMapper for JSON processing
   private final ObjectMapper jsonMapper = new ObjectMapper();
+
+  /**
+   * Creates a JWT token for the given user DTO.
+   *
+   * @param userDTO the user DTO for which the token is created
+   * @return an ObjectNode containing the JWT token and user information
+   */
   @Override
-  public ObjectNode createToken(UserDTO userDTO){
+  public ObjectNode createToken(UserDTO userDTO) {
     String token;
     try {
+      // Create the JWT token
       token = com.auth0.jwt.JWT.create().withIssuer("auth0")
           .withClaim("user", userDTO.getUserId()).sign(this.jwtAlgorithm);
+
+      // Create and return an ObjectNode containing token and user information
       return jsonMapper.createObjectNode()
           .put("token", token)
           .put("id", userDTO.getUserId())
@@ -24,9 +40,9 @@ public class JWTImpl implements JWT {
           .put("annee", userDTO.getYear())
           .put("role", userDTO.getRole().name());
     } catch (Exception e) {
-      System.out.println("Unable to create token");
+      // Log error message if token creation fails
+      System.out.println("Unable to create token: " + e.getMessage());
       return null;
     }
   }
-
 }
