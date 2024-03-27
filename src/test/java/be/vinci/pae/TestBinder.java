d22836f1b1d8dbec3d50a4738b9cc62ee5a33dd0
Alexis Cattaruzza <alexis.cattaruzza@student.vinci.be>
2024-03-27 16:52:18 +0100

    test: add new test for supervisorUCC

--- src/test/java/be/vinci/pae/TestBinder.java
+++ src/test/java/be/vinci/pae/TestBinder.java
@@ -4,6 +4,8 @@ import be.vinci.pae.dao.ContactDAO;
 import be.vinci.pae.dao.ContactDAOImpl;
 import be.vinci.pae.dao.EnterpriseDAO;
 import be.vinci.pae.dao.EnterpriseDAOImpl;
+import be.vinci.pae.dao.SupervisorDAO;
+import be.vinci.pae.dao.SupervisorDAOImpl;
 import be.vinci.pae.dao.UserDAO;
 import be.vinci.pae.dao.UserDAOImpl;
 import be.vinci.pae.domain.DomainFactory;
@@ -14,6 +16,8 @@ import be.vinci.pae.ucc.ContactUCC;
 import be.vinci.pae.ucc.ContactUCCImpl;
 import be.vinci.pae.ucc.EnterpriseUCC;
 import be.vinci.pae.ucc.EnterpriseUCCImpl;
+import be.vinci.pae.ucc.SupervisorUCC;
+import be.vinci.pae.ucc.SupervisorUCCImpl;
 import be.vinci.pae.ucc.UserUCC;
 import be.vinci.pae.ucc.UserUCCImpl;
 import be.vinci.pae.utils.DALBackService;
@@ -34,15 +38,16 @@ public class TestBinder extends AbstractBinder {
   protected void configure() {
     bind(Mockito.mock(DALServiceImpl.class)).to(DALService.class).to(DALBackService.class);
     bind(DomainFactoryImpl.class).to(DomainFactory.class);
-    bind(Mockito.mock(EnterpriseDAOImpl.class)).to(EnterpriseDAO.class);
-    bind(EnterpriseUCCImpl.class).to(EnterpriseUCC.class);
 
+    bind(Mockito.mock(EnterpriseDAOImpl.class)).to(EnterpriseDAO.class);
     bind(Mockito.mock(UserDAOImpl.class)).to(UserDAO.class);
     bind(Mockito.mock(ContactDAOImpl.class)).to(ContactDAO.class);
+    bind(Mockito.mock(SupervisorDAOImpl.class)).to(SupervisorDAO.class);
+
     bind(UserUCCImpl.class).to(UserUCC.class);
     bind(AuthUCCImpl.class).to(AuthUCC.class);
     bind(ContactUCCImpl.class).to(ContactUCC.class);
-
-
+    bind(EnterpriseUCCImpl.class).to(EnterpriseUCC.class);
+    bind(SupervisorUCCImpl.class).to(SupervisorUCC.class);
   }
 }
---
+++ src/test/java/be/vinci/pae/ucc/SupervisorUCCImplTest.java
@@ -0,0 +1,66 @@
+package be.vinci.pae.ucc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import be.vinci.pae.TestBinder;
+import be.vinci.pae.dao.SupervisorDAO;
+import be.vinci.pae.domain.DomainFactory;
+import be.vinci.pae.domain.Supervisor;
+import be.vinci.pae.utils.NotFoundException;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class SupervisorUCCImplTest {
+
+  private static ServiceLocator locator;
+  private static SupervisorDAO supervisorDAO;
+  private static DomainFactory domainFactory;
+  private static SupervisorUCC supervisorUCC;
+
+  @BeforeAll
+  static void setUp() {
+    locator = ServiceLocatorUtilities.bind(new TestBinder());
+    domainFactory = locator.getService(DomainFactory.class);
+    supervisorDAO = locator.getService(SupervisorDAO.class);
+    supervisorUCC = locator.getService(SupervisorUCC.class);
+  }
+
+  @AfterAll
+  static void tearDown() {
+    // Fermeture du ServiceLocator
+    locator.shutdown();
+  }
+
+  @Test
+  void testGetResponsibleByEnterpriseIdWithExistingSupervisor() {
+    int enterpriseId = 1;
+    Supervisor supervisor = domainFactory.getSupervisor();
+    supervisor.setEnterprise(1);
+    supervisor.setEmail("test@vinci.be");
+    supervisor.setName("test");
+    supervisor.setSurname("test");
+    supervisor.setPhone("0484752145");
+    supervisor.setEnterpriseDTO(domainFactory.getEnterprise());
+    supervisor.setResponsibleId(1);
+    when(supervisorDAO.getResponsibleByEnterpriseId(enterpriseId)).thenReturn(supervisor);
+
+    Supervisor result = supervisorUCC.getResponsibleByEnterpriseId(enterpriseId);
+
+    assertEquals(supervisor, result);
+  }
+
+  @Test
+  void testGetResponsibleByEnterpriseIdWithNonExistingSupervisor() {
+    int enterpriseId = 1;
+    when(supervisorDAO.getResponsibleByEnterpriseId(enterpriseId)).thenReturn(null);
+
+    assertThrows(NotFoundException.class, () -> {
+      supervisorUCC.getResponsibleByEnterpriseId(enterpriseId);
+    });
+  }
+}
