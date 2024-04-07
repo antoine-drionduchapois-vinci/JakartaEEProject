3a68b5f47846a444b0d58feefb82d4501967c2f0
Mwana-Nteba Yohani <yohani.mwananteba@student.vinci.be>
2024-04-07 11:00:03 +0200

    feat: adding blacklist an enterprise (backend)

--- src/main/java/be/vinci/pae/ucc/EnterpriseUCCImpl.java
+++ src/main/java/be/vinci/pae/ucc/EnterpriseUCCImpl.java
@@ -1,8 +1,12 @@
 package be.vinci.pae.ucc;
 
 import be.vinci.pae.dal.DALService;
+import be.vinci.pae.dao.ContactDAO;
 import be.vinci.pae.dao.EnterpriseDAO;
+import be.vinci.pae.domain.Enterprise;
 import be.vinci.pae.domain.EnterpriseDTO;
+import be.vinci.pae.utils.BusinessException;
+import be.vinci.pae.utils.NotFoundException;
 import jakarta.inject.Inject;
 import java.util.List;
 
@@ -12,7 +16,9 @@ import java.util.List;
 public class EnterpriseUCCImpl implements EnterpriseUCC {
 
   @Inject
-  private EnterpriseDAO enterpriseDAO;
+  private EnterpriseDAO myEnterpriseDAO;
+  @Inject
+  private ContactDAO myContactDAO;
   @Inject
   private DALService myDALService;
 
@@ -20,7 +26,7 @@ public class EnterpriseUCCImpl implements EnterpriseUCC {
   public List<EnterpriseDTO> getAllEnterprises() {
     myDALService.start();
 
-    List<EnterpriseDTO> enterpriseDTOS = enterpriseDAO.getAllEnterprises();
+    List<EnterpriseDTO> enterpriseDTOS = myEnterpriseDAO.getAllEnterprises();
     myDALService.commit();
 
     return enterpriseDTOS;
@@ -30,9 +36,37 @@ public class EnterpriseUCCImpl implements EnterpriseUCC {
   public EnterpriseDTO getEnterprisesByUserId(int userId) {
     myDALService.start();
     // get entrprise that corresponds to user intership
-    EnterpriseDTO enterpriseDTO = enterpriseDAO.getEnterpriseById(userId);
+    EnterpriseDTO enterpriseDTO = myEnterpriseDAO.getEnterpriseById(userId);
     myDALService.commit();
 
     return enterpriseDTO;
+  }
+
+  @Override
+  public EnterpriseDTO blacklistEnterprise(int enterpriseId, String blacklistedReason) {
+    myDALService.start();
+    Enterprise enterprise = (Enterprise) myEnterpriseDAO.readOne(enterpriseId);
+
+    if (enterprise.getEnterpriseId() != enterpriseId) {
+      throw new NotFoundException();
+    }
+
+    if (!enterprise.toBlacklist(blacklistedReason)) {
+      throw new BusinessException(403, "enterprise must not be already blacklisted");
+    }
+
+    EnterpriseDTO blacklistedEnterpriseDTO = myEnterpriseDAO.toBlacklist(enterprise);
+
+    //TODO
+    /*Annulation des contacts initiés avec l'entreprise
+    List<ContactDTO> suspendedContacts = myContactDAO.readEnterpriseContacts(enterpriseId);
+    for (ContactDTO c : suspendedContacts) {
+      c.
+    }*/
+    myDALService.commit();
+
+    //blacklistedEnterpriseDTO.setBlacklisted(false);
+    //blacklistedEnterpriseDTO.setBlacklistedReason(blacklistedReason);
+    return blacklistedEnterpriseDTO;
   }
 }
