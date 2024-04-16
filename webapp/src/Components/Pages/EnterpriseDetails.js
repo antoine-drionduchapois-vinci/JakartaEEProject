01fa3c5bfbed0e4287761245d8fe8e14b3a0ebf3
Alexis Cattaruzza <alexis.cattaruzza@student.vinci.be>
2024-04-16 14:38:40 +0200

    fix: center title and name row

--- webapp/src/Components/Pages/EnterpriseDetails.js
+++ webapp/src/Components/Pages/EnterpriseDetails.js
@@ -1,5 +1,7 @@
-import { clearPage, renderPageTitle } from "../../utils/render";
+import { clearPage} from "../../utils/render";
 
+let enterprise;
+const main = document.querySelector('main');
 const fetchEnterpriseContacts = async (id) => {
   console.log(id);
   try {
@@ -24,6 +26,8 @@ const fetchEnterpriseContacts = async (id) => {
           const meeting = meetingPoint || '';  // Handle null values
           const reason = refusalReason || '';
 
+          enterprise = enterpriseName;
+
           contactsHtml += `
               <tr>
               <td>${enterpriseName}</td>
@@ -44,53 +48,51 @@ const fetchEnterpriseContacts = async (id) => {
   }
 };
 
-
+const renderEnterprisePageDetails = (enterpriseContacts) => {
+  try {
+    const combinedHtml = `
+    <h2 class="title is-3 mt-3 mb-3 has-text-centered">Contact avec l'entreprise ${enterprise}</h2>
+      <table class="table is-striped is-fullwidth">
+        <thead>
+          <tr>
+            <th>Entreprise</th>
+            <th>Nom étudiant</th>
+            <th>Prénom étudiant</th>  
+            <th>Année</th>
+            <th>Lieu RDV</th>
+            <th>Etat</th>
+            <th>Raison refus</th> 
+          </tr>
+        </thead>
+        <tbody>
+          ${enterpriseContacts}
+        </tbody>
+      </table>
+    `;
+    main.innerHTML = combinedHtml;
+  } catch (error) {
+    console.error('Error rendering enterprise page:', error);
+    main.innerHTML = `<p>Error rendering data. Please try again later.</p>`;
+  }
+};
 
 const EnterpriseDetails = async () => {
-    const main = document.querySelector('main');
-    clearPage();
-    renderPageTitle('Enterprise Details');
-    const id = new URLSearchParams(window.location.search).get('id');
   
-    // Selecting main element from the DOM
-    try {
-        main.innerHTML = `<p>Loading Data...</p>`;
-        const enterpriseContacts = await fetchEnterpriseContacts(id); // Ensure this returns HTML string
+  clearPage();
+
+  const id = new URLSearchParams(window.location.search).get('id');
+
+  // Selecting main element from the DOM
+  try {
+    main.innerHTML = `<p>Loading Data...</p>`;
+    const enterpriseContacts = await fetchEnterpriseContacts(id); // Ensure this returns HTML string
+    renderEnterprisePageDetails(enterpriseContacts);
     
-        const combinedHtml = `
-        <h2 class="title is-3">Contacts de l'entreprise</h2>
-        <table class="table is-striped is-fullwidth">
-          <thead>
-            <tr>
-              <th>Entreprise</th>
-              <th>Nom étudiant</th>
-              <th>Prénom étudiant</th>  
-              <th>Année</th>
-              <th>Lieu RDV</th>
-              <th>Etat</th>
-              <th>Raison refus</th> 
-            </tr>
-          </thead>
-          <tbody>
-            ${enterpriseContacts}
-          </tbody>
-        </table>
-      </div>
-
-      
-     
-    `;
-
-        main.innerHTML = combinedHtml;
-    } catch (error) {
-        console.error('Error loading dashboard:', error);
-        // Optionally, render an error message in the main container or log it
-        main.innerHTML = `<p>Error loading data. Please try again later.</p>`;
-    }
-
-     
-
-
-}
+  } catch (error) {
+    console.error('Error loading dashboard:', error);
+    // Optionally, render an error message in the main container or log it
+    main.innerHTML = `<p>Error loading data. Please try again later.</p>`;
+  }
+};
 
 export default EnterpriseDetails;
