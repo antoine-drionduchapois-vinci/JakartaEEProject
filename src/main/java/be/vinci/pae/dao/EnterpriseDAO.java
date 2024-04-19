9179d3df5a262482c97a7eb13b970fc2cc2f0ffd
Mwana-Nteba Yohani <yohani.mwananteba@student.vinci.be>
2024-04-19 09:34:32 +0200

    fix: fix jenkins5

--- src/main/java/be/vinci/pae/dao/EnterpriseDAO.java
+++ src/main/java/be/vinci/pae/dao/EnterpriseDAO.java
@@ -4,28 +4,35 @@ import be.vinci.pae.domain.EnterpriseDTO;
 import java.util.List;
 
 /**
- * The EnterpriseDAO interface provides methods for accessing enterprise information from the
+ * The EnterpriseDAO interface provides methods for accessing enterprise
+ * information from the
  * database.
  */
 public interface EnterpriseDAO {
 
   /**
-   * Retrieves the enterprise information associated with the specified enterprise ID.
+   * Retrieves the enterprise information associated with the specified enterprise
+   * ID.
    *
    * @param enterpriseId The ID of the enterprise to retrieve information for.
-   * @return An EnterpriseDTO object representing the enterprise information associated with the
-   * specified ID, or null if no enterprise with the given ID is found.
+   * @return An EnterpriseDTO object representing the enterprise information
+   *         associated with the
+   *         specified ID, or null if no enterprise with the given ID is found.
    */
   EnterpriseDTO readOne(int enterpriseId);
 
   /**
-   * Retrieves the enterprise information associated with the specified enterprise name and
+   * Retrieves the enterprise information associated with the specified enterprise
+   * name and
    * enterprise label.
    *
-   * @param enterpriseName  The name of the enterprise to retrieve information for.
-   * @param enterpriseLabel The label of the enterprise to retrieve information for.
-   * @return An EnterpriseDTO object representing the enterprise information associated with the
-   * specified ID, or null if no enterprise with the given ID is found.
+   * @param enterpriseName  The name of the enterprise to retrieve information
+   *                        for.
+   * @param enterpriseLabel The label of the enterprise to retrieve information
+   *                        for.
+   * @return An EnterpriseDTO object representing the enterprise information
+   *         associated with the
+   *         specified ID, or null if no enterprise with the given ID is found.
    */
   EnterpriseDTO readOne(String enterpriseName, String enterpriseLabel);
 
@@ -44,8 +51,9 @@ public interface EnterpriseDAO {
   /**
    * Retrieves a list of all enterprises stored in the database.
    *
-   * @return A List containing EnterpriseDTO objects representing all enterprises stored in the
-   * database.
+   * @return A List containing EnterpriseDTO objects representing all enterprises
+   *         stored in the
+   *         database.
    */
   List<EnterpriseDTO> getAllEnterprises();
 
@@ -53,15 +61,17 @@ public interface EnterpriseDAO {
    * Retrieves the enterprise information associated with the specified ID.
    *
    * @param id The ID of the enterprise to retrieve information for.
-   * @return An EnterpriseDTO object representing the enterprise information associated with the
-   * specified ID, or null if no enterprise with the given ID is found.
+   * @return An EnterpriseDTO object representing the enterprise information
+   *         associated with the
+   *         specified ID, or null if no enterprise with the given ID is found.
    */
   EnterpriseDTO getEnterpriseById(int id);
 
   /**
    * Blacklist an enterprise.
    *
-   * @param blacklistedEnterpriseDTO the EnterpriseDTO of the enterprise to blacklist
+   * @param blacklistedEnterpriseDTO the EnterpriseDTO of the enterprise to
+   *                                 blacklist
    * @return The enterprise blacklisted
    */
   EnterpriseDTO toBlacklist(EnterpriseDTO blacklistedEnterpriseDTO);
